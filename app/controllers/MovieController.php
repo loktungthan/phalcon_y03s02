c8a7ad49761a6a2d792c3f0282eb945ca1372028
loktungthan <38791850+loktungthan@users.noreply.github.com>
2018-04-27 20:26:38 +0100

    added movie crud

---
+++ app/controllers/MovieController.php
@@ -0,0 +1,239 @@
+<?php
+ 
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
+
+class MovieController extends ControllerBase
+{
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+        $this->persistent->parameters = null;
+    }
+
+    /**
+     * Searches for movie
+     */
+    public function searchAction()
+    {
+        $numberPage = 1;
+        if ($this->request->isPost()) {
+            $query = Criteria::fromInput($this->di, 'Movie', $_POST);
+            $this->persistent->parameters = $query->getParams();
+        } else {
+            $numberPage = $this->request->getQuery("page", "int");
+        }
+
+        $parameters = $this->persistent->parameters;
+        if (!is_array($parameters)) {
+            $parameters = [];
+        }
+        $parameters["order"] = "id";
+
+        $movie = Movie::find($parameters);
+        if (count($movie) == 0) {
+            $this->flash->notice("The search did not find any movie");
+
+            $this->dispatcher->forward([
+                "controller" => "movie",
+                "action" => "index"
+            ]);
+
+            return;
+        }
+
+        $paginator = new Paginator([
+            'data' => $movie,
+            'limit'=> 10,
+            'page' => $numberPage
+        ]);
+
+        $this->view->page = $paginator->getPaginate();
+    }
+
+    /**
+     * Displays the creation form
+     */
+    public function newAction()
+    {
+
+    }
+
+    /**
+     * Edits a movie
+     *
+     * @param string $id
+     */
+    public function editAction($id)
+    {
+        if (!$this->request->isPost()) {
+
+            $movie = Movie::findFirstByid($id);
+            if (!$movie) {
+                $this->flash->error("movie was not found");
+
+                $this->dispatcher->forward([
+                    'controller' => "movie",
+                    'action' => 'index'
+                ]);
+
+                return;
+            }
+
+            $this->view->id = $movie->getId();
+
+            $this->tag->setDefault("id", $movie->getId());
+            $this->tag->setDefault("name", $movie->getName());
+            $this->tag->setDefault("description", $movie->getDescription());
+            $this->tag->setDefault("rating", $movie->getRating());
+            $this->tag->setDefault("image", $movie->getImage());
+            
+        }
+    }
+
+    /**
+     * Creates a new movie
+     */
+    public function createAction()
+    {
+        if (!$this->request->isPost()) {
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        $movie = new Movie();
+        $movie->setname($this->request->getPost("name"));
+        $movie->setdescription($this->request->getPost("description"));
+        $movie->setrating($this->request->getPost("rating"));
+        $movie->setimage(base64_encode(file_get_contents($this->request->getUploadedFiles()[0]->getTempName())));
+        
+
+        if (!$movie->save()) {
+            foreach ($movie->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'new'
+            ]);
+
+            return;
+        }
+
+        $this->flash->success("movie was created successfully");
+
+        $this->dispatcher->forward([
+            'controller' => "movie",
+            'action' => 'index'
+        ]);
+    }
+
+    /**
+     * Saves a movie edited
+     *
+     */
+    public function saveAction()
+    {
+
+        if (!$this->request->isPost()) {
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        $id = $this->request->getPost("id");
+        $movie = Movie::findFirstByid($id);
+
+        if (!$movie) {
+            $this->flash->error("movie does not exist " . $id);
+
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        $movie->setname($this->request->getPost("name"));
+        $movie->setdescription($this->request->getPost("description"));
+        $movie->setrating($this->request->getPost("rating"));
+        $movie->setimage($this->request->getPost("image"));
+        
+
+        if (!$movie->save()) {
+
+            foreach ($movie->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'edit',
+                'params' => [$movie->getId()]
+            ]);
+
+            return;
+        }
+
+        $this->flash->success("movie was updated successfully");
+
+        $this->dispatcher->forward([
+            'controller' => "movie",
+            'action' => 'index'
+        ]);
+    }
+
+    /**
+     * Deletes a movie
+     *
+     * @param string $id
+     */
+    public function deleteAction($id)
+    {
+        $movie = Movie::findFirstByid($id);
+        if (!$movie) {
+            $this->flash->error("movie was not found");
+
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'index'
+            ]);
+
+            return;
+        }
+
+        if (!$movie->delete()) {
+
+            foreach ($movie->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            $this->dispatcher->forward([
+                'controller' => "movie",
+                'action' => 'search'
+            ]);
+
+            return;
+        }
+
+        $this->flash->success("movie was deleted successfully");
+
+        $this->dispatcher->forward([
+            'controller' => "movie",
+            'action' => "index"
+        ]);
+    }
+
+}
---
+++ app/models/Movie.php
@@ -0,0 +1,199 @@
+<?php
+
+class Movie extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     * @Primary
+     * @Identity
+     * @Column(column="id", type="integer", length=11, nullable=false)
+     */
+    protected $id;
+
+    /**
+     *
+     * @var string
+     * @Column(column="name", type="string", length=30, nullable=true)
+     */
+    protected $name;
+
+    /**
+     *
+     * @var string
+     * @Column(column="description", type="string", length=30, nullable=true)
+     */
+    protected $description;
+
+    /**
+     *
+     * @var integer
+     * @Column(column="rating", type="integer", length=11, nullable=true)
+     */
+    protected $rating;
+
+    /**
+     *
+     * @var string
+     * @Column(column="image", type="string", nullable=true)
+     */
+    protected $image;
+
+    /**
+     * Method to set the value of field id
+     *
+     * @param integer $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field name
+     *
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field description
+     *
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field rating
+     *
+     * @param integer $rating
+     * @return $this
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field image
+     *
+     * @param string $image
+     * @return $this
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the value of field name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Returns the value of field description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Returns the value of field rating
+     *
+     * @return integer
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Returns the value of field image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("ca_y03s02");
+        $this->setSource("movie");
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'movie';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Movie[]|Movie|\Phalcon\Mvc\Model\ResultSetInterface
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Movie|\Phalcon\Mvc\Model\ResultInterface
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
